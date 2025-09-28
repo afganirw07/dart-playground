b7f119d0bf67038d9e1c3cdd39c44929a8a9fd2f
afganirw07 <afganirwansyah070708@gmail.com>
2025-09-28 22:04:35 +0700

    infinite loop

--- bin/loop.dart
+++ bin/loop.dart
@@ -6,6 +6,8 @@ void main() {
   for (var i = 0; i < siswa.length; i++) {
     print("Siswa ${siswa[i]}");
   }
+  
+  infiniteLoop();
 }
 
 const siswa = [
@@ -20,3 +22,9 @@ const siswa = [
   "Indra",
   "Joko",
 ];
+
+void infiniteLoop() {
+  while (true) {
+    print("Loading...");
+  }
+}
