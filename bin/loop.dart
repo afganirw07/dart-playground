21d6d154550dde55b377b97471aa74aa8c73d494
afganirw07 <afganirwansyah070708@gmail.com>
2025-09-28 21:56:01 +0700

    loop

---
+++ bin/loop.dart
@@ -0,0 +1,22 @@
+void main() {
+  for (int i = 1; i <= 100; i++) {
+    print("Angka ke $i");
+  }
+
+  for (var i = 0; i < siswa.length; i++) {
+    print("Siswa ${siswa[i]}");
+  }
+}
+
+const siswa = [
+  "Andi",
+  "Budi",
+  "Caca",
+  "Deni",
+  "Eka",
+  "Fani",
+  "Gina",
+  "Hani",
+  "Indra",
+  "Joko",
+];
